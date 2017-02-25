640645199beb8b45e1e5b202ffa3393ba6289775
midorigoke <midorigoke@desumachi.tk>
2017-02-25 19:07:58 +0900

    add: SampleManager

---
+++ YukarinetteSamplePlugin/SampleManager.cs
@@ -0,0 +1,20 @@
+﻿namespace YukarinetteSamplePlugin
+{
+	public class SampleManager
+	{
+		public void Create(string sampleSetting)
+		{
+
+		}
+
+		public void Dispose()
+		{
+
+		}
+
+		public void Speech(string text)
+		{
+
+		}
+	}
+}
